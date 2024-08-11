89412413b00a1b47c9e18163c031e2d1f622ee1f
yuvalkalan <yuvalkalan@gmail.com>
2024-08-12 02:39:16 +0300

    use dma library in NeoPio

--- led_strip/neopio.py
+++ led_strip/neopio.py
@@ -2,6 +2,7 @@ from .color import BLACK
 import machine
 from .constants import *
 import rp2
+from .dma import DMA
 
 
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
@@ -64,19 +65,20 @@ def neopio_asm():
 
 class NeoPio:
     def __init__(self, pin):
-        self._values = [to_bin(BLACK)] * NUM_OF_PIXELS
+        self._dma = DMA(NUM_OF_PIXELS)
         self._sm = rp2.StateMachine(0, neopio_asm, freq=20_000_000, set_base=machine.Pin(pin))
         self._sm.active(1)
 
     def write(self):
-        for v in self._values:
-            self._sm.put(v)
+        self._dma.start()
+        # for v in self._dma:
+        #     self._sm.put(v)
 
     def quit(self):
         self._sm.active(0)
 
     def __setitem__(self, key, value):
-        self._values[key] = to_bin(value)
+        self._dma[key] = to_bin(value)
 
 
 def to_bin(color):

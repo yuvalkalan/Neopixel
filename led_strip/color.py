928bd4595412cfbd27109bf07da6391395e549bb
yuvalkalan <yuvalkalan@gmail.com>
2024-08-12 13:54:57 +0300

    update color generate

--- led_strip/color.py
+++ led_strip/color.py
@@ -1,22 +1,24 @@
 import random
 import time
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-MAGENTA = (255, 0, 255)
-AQUA = (0, 255, 255)
-COLORS = [RED, GREEN, BLUE, YELLOW, MAGENTA, AQUA]
-
-
-def led_fade(start, end, delta):
-    return int(start + (end - start) * delta)
+from .neopio import BaseColor
 
 
 NS_TIME = 10 ** 9
+
+
+def led_fade(start, end, delta):
+    return max(0, min(int(start + (end - start) * delta), 255))
+
+
+BLACK = BaseColor((0, 0, 0))
+WHITE = BaseColor((255, 255, 255))
+RED = BaseColor((255, 0, 0))
+GREEN = BaseColor((0, 255, 0))
+BLUE = BaseColor((0, 0, 255))
+YELLOW = BaseColor((255, 255, 0))
+MAGENTA = BaseColor((255, 0, 255))
+AQUA = BaseColor((0, 255, 255))
+COLORS = [RED, GREEN, BLUE, YELLOW, MAGENTA, AQUA]
 
 
 class Color:
@@ -42,17 +44,8 @@ class Color:
             self._gen()
             delta = (time.time_ns() - self._start_time) / NS_TIME
         brightness = max_bright / 255
-        #         print(brightness)
-        #         print(self._color, self._next_color, delta)
         fade_level = delta / duration
-        new_color = tuple([led_fade(brightness * self._color[i],
-                                    brightness * self._next_color[i],
-                                    fade_level) for i in range(3)])
-        #         print(new_color)
-
-        #         if new_color == self._next_color:
-        #             self._gen()
-        # real color after reduce brightness
-        #         real_color = tuple([int(v * max_bright / 255) for v in new_color])
+        new_color = BaseColor([led_fade(brightness*self._color[i],
+                                        brightness*self._next_color[i],
+                                        fade_level) for i in range(3)])
         return new_color
-
